58ac122ee8d97486d5e3cea977e013ab38561472
PatMis16 <patrick_mischler@icloud.com>
2021-04-22 08:04:51 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,49 +2,31 @@
 
 set -eux
 
-if [ -z "${CMD_PATH+x}" ]; then
-  echo "::warning file=entrypoint.sh,line=6,col=1::CMD_PATH not set"
-  export CMD_PATH=""
-fi
-
-FILE_LIST=`/build.sh`
-
-#echo "::warning file=/build.sh,line=1,col=5::${FILE_LIST}"
+/build.sh
 
 EVENT_DATA=$(cat $GITHUB_EVENT_PATH)
 echo $EVENT_DATA | jq .
 UPLOAD_URL=$(echo $EVENT_DATA | jq -r .release.upload_url)
 UPLOAD_URL=${UPLOAD_URL/\{?name,label\}/}
 RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
-#PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
-PROJECT_NAME=$(echo $GITHUB_REPOSITORY | awk -F/ '{print $2}')
-NAME="${NAME:-${PROJECT_NAME}_${RELEASE_NAME}}_${GOOS}_${GOARCH}"
+PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
+NAME="${PROJECT_NAME}_${RELEASE_NAME}_${GOOS}_${GOARCH}"
 
-if [ -z "${EXTRA_FILES+x}" ]; then
-echo "::warning file=entrypoint.sh,line=22,col=1::EXTRA_FILES not set"
-fi
-
-FILE_LIST="${FILE_LIST} ${EXTRA_FILES}"
-
-FILE_LIST=`echo "${FILE_LIST}" | awk '{$1=$1};1'`
-
+EXT=''
 
 if [ $GOOS == 'windows' ]; then
-ARCHIVE=tmp.zip
-zip -9r $ARCHIVE ${FILE_LIST}
-else
-ARCHIVE=tmp.tgz
-tar cvfz $ARCHIVE ${FILE_LIST}
+  EXT='.exe'
 fi
 
-CHECKSUM=$(md5sum ${ARCHIVE} | cut -d ' ' -f 1)
+tar cvfz tmp.tgz "${PROJECT_NAME}${EXT}"
+CHECKSUM=$(md5sum tmp.tgz | cut -d ' ' -f 1)
 
 curl \
   -X POST \
-  --data-binary @${ARCHIVE} \
-  -H 'Content-Type: application/octet-stream' \
+  --data-binary @tmp.tgz \
+  -H 'Content-Type: application/gzip' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${NAME}.${ARCHIVE/tmp./}"
+  "${UPLOAD_URL}?name=${NAME}.tar.gz"
 
 curl \
   -X POST \
